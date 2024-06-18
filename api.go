00978e6afce99ad33431a13cd7f5b9043fefff56
Nirrax <Nirraxx@gmail.com>
2024-06-18 04:58:00 +0200

    feat: Add route and handler for Delete method

--- api.go
+++ api.go
@@ -8,10 +8,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func writeJson(w http.ResponseWriter, status int, v any) error {
+func writeJson(w http.ResponseWriter, status int, value any) error {
 	w.WriteHeader(status)
 	w.Header().Set("Content-Type", "application/json")
-	return json.NewEncoder(w).Encode(v)
+	return json.NewEncoder(w).Encode(value)
 }
 
 type apiFunc func(http.ResponseWriter, *http.Request) error
@@ -23,6 +23,7 @@ type ApiError struct {
 // Defers error for handler functions
 func makeHttpHandleFunc(f apiFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// If handler function has an error, handle the error
 		if err := f(w, r); err != nil {
 			writeJson(w, http.StatusBadRequest, ApiError{Error: err.Error()})
 		}
@@ -33,6 +34,7 @@ type ApiServer struct {
 	address string
 }
 
+// Constructor
 func NewApiServer(address string) *ApiServer {
 	return &ApiServer{
 		address: address,
@@ -44,6 +46,7 @@ func (s *ApiServer) Run() {
 
 	router.HandleFunc("/{url}", makeHttpHandleFunc(s.handleGetUrl)).Methods("GET")
 	router.HandleFunc("/{url}", makeHttpHandleFunc(s.handleCreateUrl)).Methods("POST")
+	router.HandleFunc("/{url}", makeHttpHandleFunc(s.handleDeleteUrl)).Methods("DELETE")
 	router.HandleFunc("/info/{url}", makeHttpHandleFunc(s.handleGetUrlInfo)).Methods("GET")
 
 	log.Println("Running on port: " + s.address)
@@ -52,7 +55,7 @@ func (s *ApiServer) Run() {
 }
 
 func (s *ApiServer) handleGetUrl(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	return writeJson(w, http.StatusOK, "Get works")
 }
 
 func (s *ApiServer) handleGetUrlInfo(w http.ResponseWriter, r *http.Request) error {
@@ -60,5 +63,9 @@ func (s *ApiServer) handleGetUrlInfo(w http.ResponseWriter, r *http.Request) err
 }
 
 func (s *ApiServer) handleCreateUrl(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *ApiServer) handleDeleteUrl(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
