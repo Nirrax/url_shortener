a84fa875017145fc7558cb9147a30ed9010c2e7a
Nirrax <nirraxx@gmail.com>
2026-03-31 00:20:38 +0200

    feat: Add package that loads config

---
+++ internal/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	ServerPort     string
+	RunUpMigration bool
+	PostgresConfig
+}
+
+type PostgresConfig struct {
+	DBHost     string
+	DBport     string
+	DBuser     string
+	DBpassword string
+	DBname     string
+}
+
+func LoadConfig() Config {
+	viper.SetConfigFile(".env")
+	viper.SetConfigType("env")
+
+	// prefere loaded variables than from file
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Println("No config file found")
+	}
+
+	return Config{
+		ServerPort:     viper.GetString("SERVER_PORT"),
+		RunUpMigration: viper.GetBool("RUN_UP_MIGRATION"),
+
+		PostgresConfig: PostgresConfig{
+			DBHost:     viper.GetString("DB_HOST"),
+			DBport:     viper.GetString("DB_PORT"),
+			DBuser:     viper.GetString("DB_USER"),
+			DBpassword: viper.GetString("DB_PASSWORD"),
+			DBname:     viper.GetString("DB_NAME"),
+		},
+	}
+}
